8d0516ca996073fbf0bee8b8803bf84ddea22d6f
= <=>
2025-03-09 14:04:06 -0300

    Testado o Método Atualizar Cliente

--- src/main/java/com/eymatsuda/clientes/rest/ClienteController.java
+++ src/main/java/com/eymatsuda/clientes/rest/ClienteController.java
@@ -25,25 +25,36 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente){
+    public Cliente salvar(@RequestBody Cliente cliente) {
 
         return repository.save(cliente);
     }
 
     @GetMapping("{id}")
-    public Cliente localizarPorId (@PathVariable Integer id){
+    public Cliente localizarPorId(@PathVariable Integer id) {
         return repository
                 .findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("{id}")
-    public void deletar(@PathVariable Integer id){
+    public void deletar(@PathVariable Integer id) {
         repository
                 .findById(id).map(cliente -> {
-                    repository.delete(cliente);
-                    return Void.TYPE;
+            repository.delete(cliente);
+            return Void.TYPE;
         })
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado) {
+        repository.findById(id).map(cliente -> {
+            cliente.setNome(clienteAtualizado.getNome());
+            cliente.setCpf((clienteAtualizado.getCpf()));
+            return repository.save(clienteAtualizado);
+        })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
